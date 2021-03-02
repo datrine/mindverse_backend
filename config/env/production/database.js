be5b07e40b84e7a85052b7fd6cfd0d92f89505c7
datrine <trinitietp@gmail.com>
2021-03-02 16:05:31 +0100

    re-initial

---
+++ config/env/production/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'wgh9.whogohost.com'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'medikedu_mindversedb'),
+        username: env('DATABASE_USERNAME', 'medikedu_datrine'),
+        password: env('DATABASE_PASSWORD', 'TeMi4ToPe'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
